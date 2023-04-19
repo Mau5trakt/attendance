9bd394abb6ed505d6a0a4a26126aa00c0c832786
Mau5trakt <kubzalemany@hotmail.com>
2023-04-19 03:09:09 -0600

    feat: created functions.py

---
+++ functions.py
@@ -0,0 +1,13 @@
+from flask import redirect, session
+from functools import wraps
+def login_required(f):
+    """
+    Decorate routes to require login.
+    https://flask.palletsprojects.com/en/1.1.x/patterns/viewdecorators/
+    """
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("codigo") is None:
+            return redirect("/iniciar")
+        return f(*args, **kwargs)
+    return decorated_function
