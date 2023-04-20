8365d0a4490435b57c822ab4d02ab17c690f424a
Mau5trakt <kubzalemany@hotmail.com>
2023-04-20 14:37:52 -0600

    feature:! created the functions to manage the Class route data

--- static/script.js
+++ static/script.js
@@ -5,6 +5,8 @@ var day = []; //listas de dias
 var bloq = []; // listas de bloques
 var cumpli = []; //lista de cumplimiento
 var coment = []; //lista de comentarios
+var lecture = [];
+var a_j = [];
 
 
 
@@ -65,6 +67,33 @@ function enviarOH(){
     location.reload();
 }
 
+
+function agregarClass() {
+        event.preventDefault(); // Evita que el formulario se envíe
+        //Grupo, Nombre, Lecture, A_J, Comentario
+        // Obtén los valores seleccionados del select y el input
+
+        var grupo = document.getElementById('grupo').value;
+        var estudiante = document.getElementById('seleccion-estudiantes').value;
+        var clase = document.getElementById('nClass').value;
+        var accion = document.getElementById('a_j').value;
+        var comentario = document.getElementById('Comentario').value;
+
+        var nuevoElemento = document.createElement('li');
+        nuevoElemento.textContent = `Grupo: ${grupo}, Estudiante: ${estudiante}, Clase: ${clase},
+         [A]/[J]: ${accion},Comentario: ${comentario}`
+        document.getElementById('listaEstudiantes').appendChild(nuevoElemento);
+
+        grup.push(grupo);
+        est.push(estudiante);
+        lecture.push(clase);
+        a_j.push(accion)
+        coment.push(comentario);
+
+        //document.getElementById('grupo').value = '';
+        document.getElementById('seleccion-estudiantes').value = '';
+}
+
 function enviarClass(){
     event.preventDefault();
      let url = "/procesarClass";
@@ -85,38 +114,4 @@ function enviarClass(){
 
     alert(`Datos enviados exitosamente, se enviarion ${est.length} estudiantes`);
     location.reload();
-}
-function agregarClass() {
-        event.preventDefault(); // Evita que el formulario se envíe
-
-        // Obtén los valores seleccionados del select y el input
-
-        var grupo = document.getElementById('grupo').value;
-        var estudiante = document.getElementById('seleccion-estudiantes').value;
-        var semana = document.getElementById('Semana').value;
-        var dia = document.getElementById('Dsemana').value;
-        var bloque = document.getElementById('Bloque').value;
-        var cumplimiento = document.getElementById('Cumplimiento').value;
-        var comentario = document.getElementById('Comentario').value;
-
-
-
-        // Crea un nuevo elemento de lista y añade los valores a la lista
-        var nuevoElemento = document.createElement('li');
-        nuevoElemento.textContent = `Grupo: ${grupo}, Estudiante: ${estudiante}, Semana: ${semana},
-        Dia: ${dia}, Bloque: ${bloque}, Cumplimiento: ${cumplimiento}, Comentario: ${comentario}`
-        document.getElementById('listaEstudiantes').appendChild(nuevoElemento);
-
-
-        est.push(estudiante);
-        grup.push(grupo);
-        sem.push(semana);
-        day.push(dia);
-        bloq.push(bloque);
-        cumpli.push(cumplimiento);
-        coment.push(comentario);
-
-
-        //document.getElementById('grupo').value = '';
-        document.getElementById('seleccion-estudiantes').value = '';
 }
