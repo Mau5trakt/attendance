c8178910bdb3fa6acf361f8247a383c03cfb06d2
Mau5trakt <kubzalemany@hotmail.com>
2023-04-20 22:26:53 -0600

    refactor: testing the way to open files

--- prueb.py
+++ prueb.py
@@ -1,4 +1,9 @@
 from datetime import  datetime
 
-now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-print(now)
+#now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+#print(now)
+
+with open('cred.txt', 'r') as f:
+    primera_linea = f.readline()
+
+print(primera_linea)
