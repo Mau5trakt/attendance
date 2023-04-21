537c5d61d475c481716a31a514284834e2a67fa9
Mau5trakt <kubzalemany@hotmail.com>
2023-04-20 19:28:03 -0600

    feat: Created the route to process the Class Attendance and removed some unused imports

--- app.py
+++ app.py
@@ -3,11 +3,7 @@ from database import db
 from flask import Flask, render_template, request, url_for
 from flask_migrate import Migrate
 from flask_session import Session
-#from Forms import *
-#from helpers import *
 from Models import *
-from sqlalchemy import or_, desc
-from tempfile import mkdtemp
 from werkzeug.security import check_password_hash, generate_password_hash
 app = Flask(__name__)
 
@@ -81,7 +77,34 @@ def procesar():
     return redirect(url_for("inicio"))
 
 
+@app.route('/procesarClass', methods=["GET", "POST"])
+def procesarClass():
+    data = request.get_json()
+    estudiante = data.get("estudiantes")
+    grupo = data.get("grupos")
+    lectures = data.get("lectures")
+    a_j = data.get("a_j")
+    comentario = data.get("comentario")
 
+    staff_id = 1  # ! Obtenerlo del login
+    for a in range(len(estudiante)):
+        print(f"Estudiante: {estudiante[a]} Grupo: {grupo[a]} Lecture: {lectures[a]} A/J = [{a_j[a]}]")
+        obj_estudiante = Estudiantes.query.filter_by(student=estudiante[a]).first()
+        if obj_estudiante:
+            insercion = Registros(staff_id=staff_id,
+                                  student_id=obj_estudiante.id,
+                                  cicle=obj_estudiante.cicle,
+                                  group=obj_estudiante.group,
+                                  student_name=obj_estudiante.student,
+                                  type="C",
+                                  a_j= a_j[a],
+                                  lecture=lectures[a])
+            db.session.add(insercion)
+            db.session.commit()
+
+
+
+    return(redirect(url_for("inicio")))
 
 if __name__ == '__main__':
     app.run()
