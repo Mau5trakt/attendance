41331e2b675e6ab65f7d2790f66a8a3de10b9a83
Mau5trakt <80871251+Mau5trakt@users.noreply.github.com>
2023-05-17 17:02:23 +0000

    feat: created registers views

--- app.py
+++ app.py
@@ -1,18 +1,25 @@
 from flask import Flask, redirect, render_template_string
+from consultas import *
 from database import db
-from flask import Flask, render_template, request, url_for
+from flask import Flask, render_template, request, url_for, session
 from flask_migrate import Migrate
 from flask_session import Session
+from functions import *
+from tempfile import mkdtemp
 from Models import *
 from sqlalchemy import desc
 from werkzeug.security import check_password_hash, generate_password_hash
+import os
+
 app = Flask(__name__)
 
 
-with open('cred.txt', 'r') as f:
-    primera_linea = f.readline()
 
-app.config['SQLALCHEMY_DATABASE_URI'] = primera_linea
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+        'sqlite:///' + os.path.join(basedir, 'attendamce.db') #TODO: Change the name for attendance
+
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False #   Trackear las modificaciones realizadas
 db.init_app(app)
 
@@ -30,7 +37,53 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 
-@app.route('/ohAttendance', methods=["GET", "POST"])
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+
+@app.route('/iniciar', methods=["GET", "POST"])
+def login():
+    session.clear()
+    if request.method =="POST":
+        if not request.form.get("user"):
+            return apology("Ingrese su usuario", 403)
+
+        if not request.form.get("password"):
+            return apology("Ingrese su contraseña", 403)
+
+        user = request.form.get("user")
+        passw = request.form.get("password")
+
+        print("user", user)
+
+        staff = Staff.query.filter_by(code=user).first()
+        print("staff", staff)
+
+        if staff is None or not check_password_hash(staff.password, passw):
+            return apology("Usuario o Clave Incorrecto", 403)
+
+        session["code"] = staff.code
+
+        return render_template("ohattendance.html")
+
+
+
+
+
+
+    return render_template('login.html')  # put application's code here
+
+
+
+# Ruta principal
+
+@app.route('/', methods=["GET", "POST"])
+@login_required
 def inicio():  # put application's code here
     estudiantes = Estudiantes.query.order_by('id')
     if request.method == "POST":
@@ -40,11 +93,13 @@ def inicio():  # put application's code here
     return render_template('ohattendance.html', estudiantes=estudiantes)
 
 @app.route('/classAttendance', methods=["GET", "POST"])
+@login_required
 def classAttendance():  # put application's code here
     estudiantes = Estudiantes.query.order_by('id')
     return render_template('classAttendance.html', estudiantes=estudiantes)
 
 @app.route('/procesarOH', methods=["GET", "POST"])
+@login_required
 def procesar():
     data = request.get_json()
     estudiante = data.get("estudiantes")
@@ -54,7 +109,7 @@ def procesar():
     bloque = data.get("bloque")
     cumplimiento = data.get("cumplimiento")
     comentario = data.get("comentario")
-    staff_id = 1 #! Obtenerlo del login
+    staff_id = session["code"] #! Obtenerlo del login
 
     for a in range(len(estudiante)):
         obj_estudiante = Estudiantes.query.filter_by(student=estudiante[a]).first()
@@ -80,6 +135,7 @@ def procesar():
 
 
 @app.route('/procesarClass', methods=["GET", "POST"])
+@login_required
 def procesarClass():
     data = request.get_json()
     estudiante = data.get("estudiantes")
@@ -88,7 +144,7 @@ def procesarClass():
     a_j = data.get("a_j")
     comentario = data.get("comentario")
 
-    staff_id = 1  # ! Obtenerlo del login
+    staff_id = session["code"]  # ! Obtenerlo del login
     for a in range(len(estudiante)):
         print(f"Estudiante: {estudiante[a]} Grupo: {grupo[a]} Lecture: {lectures[a]} A/J = [{a_j[a]}]")
         obj_estudiante = Estudiantes.query.filter_by(student=estudiante[a]).first()
@@ -111,14 +167,37 @@ def procesarClass():
     return(redirect(url_for("inicio")))
 
 @app.route('/registros', methods=["GET", "POST"])
-def allregisters():  # put application's code here
+@login_required
+def allregisters():
     registros = Registros.query.order_by(desc('id')).limit(150).all()
+    query = db.session.execute(reporteOhMembers).fetchall()
 
     for registro in registros:
         print(registro.id)
 
+    print(type(registros))
 
-    return render_template('registros.html', registros=registros)
+    return render_template('registros.html', registros=registros, query=query)
+
+@app.route('/membersOH', methods=["GET", "POST"])
+@login_required
+def members():
+    #query = db.session.execute('SELECT estudiantes.*, COALESCE(sum(registros.duration), 0) as cantidad FROM estudiantes LEFT JOIN registros ON estudiantes.student = registros.student_name WHERE estudiantes.cicle = "Y23C1" GROUP BY estudiantes.student ORDER BY estudiantes.id;').fetchall()
+    query = db.session.execute(reporteOhMembers).fetchall()
+    #reporteOhMembers es la consulta importada de `consultas.py`
+    for consulta in query:
+        print(consulta)
+
+    print(type(query))
+
+
+
+
+
+
+
+    return render_template('membersOH.html', query=query)
+
 
 if __name__ == '__main__':
     app.run()
